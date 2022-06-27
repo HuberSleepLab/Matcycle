6f1133bc87ee0d43ed601f1636acf7a07246ca82
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-06-27 21:30:47 +0200

    now loops through requested repeats, and saves diagnostic figure

--- PinkNoiseTest.m
+++ PinkNoiseTest.m
@@ -2,42 +2,10 @@ close all
 clear
 clc
 
-
-%% generate signal
-
 fs = 250;
 Minutes = 10;
+nPoints = fs*60*Minutes;
 
-
-L = fs*60*Minutes;
-c = linspace(1, fs, L/2);
-freqs = fs*(0:(L/2))/L;
-S = 1./c;
-freqs = freqs(1:end-1);
-figure;plot(freqs, S)
-
-
-S(L/2+1:L)=flip(S);
-S=S.*exp(j*2*pi*rand(1, L));
-figure
-Power = abs(S);
-plot(freqs, Power(1:L/2))
-S(1)=0;
-
-t = linspace(0, L/fs, L);
-figure
-Signal = real(ifft(S));
-plot(t, Signal)
-
-
-Signal = hpfilt(Signal, fs, 2);
-Signal = lpfilt(Signal, fs, 40);
-hold on;plot(t, Signal)
-
-%%
-
-
-nPoints = L;
 Min_Peaks = 3; % minimum number of cycles per burst
 
 % Burst Thresholds for finding very clean bursts
@@ -65,42 +33,90 @@ Bands.Theta = [4 8];
 Bands.ThetaAlpha = [6 10];
 Bands.Alpha = [8 12];
 
-Keep_Points = ones(1, nPoints); % set to 0 any points that contain artifacts or just wish to ignore.
+
+Repeats = 4;
+
+NBursts = zeros(1, Repeats);
+periods = [];
+npeaks = [];
+
+for Indx_R = 1:Repeats
+
+    %% generate signal
+
+    c = linspace(1, fs, nPoints/2);
+    freqs = fs*(0:(nPoints/2))/nPoints;
+    S = 1./c;
+    % freqs = freqs(1:end-1);
+    % figure;plot(freqs, S)
 
 
-%% look at bursts
-BandNames = fieldnames(Bands);
+    S(nPoints/2+1:nPoints)=flip(S);
+    S=S.*exp(j*2*pi*rand(1, nPoints));
+    % figure
+    Power = abs(S);
+    % plot(freqs, Power(1:L/2))
+    S(1)=0;
 
-EEG = struct();
-EEG.data = Signal;
-EEG.srate = fs;
-FiltEEG = EEG;
+    t = linspace(0, nPoints/fs, nPoints);
+    % figure
+    Signal = real(ifft(S));
+    % plot(t, Signal)
 
-for Indx_F = 1:numel(BandNames)
-    B = Bands.(BandNames{Indx_F});
-    fSignal = hpfilt(Signal, fs, B(1));
-    fSignal = lpfilt(fSignal, fs, B(2));
+    % filter between 2 and 40 hz
+    Signal = hpfilt(Signal, fs, 2);
+    Signal = lpfilt(Signal, fs, 40);
+%     hold on;plot(t, Signal)
 
-    Peaks = peakDetection(Signal, fSignal);
-    Peaks = peakProperties(Signal, Peaks, fs);
-    BT = removeEmptyFields(BurstThresholds(1));
-    [~, BurstPeakIDs_Clean] = findBursts(Peaks, BT, Min_Peaks, Keep_Points);
-    plotBursts(Signal, fs, Peaks, BurstPeakIDs_Clean, BT)
+    %%
 
-    BT = removeEmptyFields(BurstThresholds(2));
-    [~, BurstPeakIDs_Clean] = findBursts(Peaks, BT, Min_Peaks, Keep_Points);
-    plotBursts(Signal, fs, Peaks, BurstPeakIDs_Clean, BT)
 
-    % count bursts
-    FiltEEG(Indx_F).data = fSignal;
-end
+    Keep_Points = ones(1, nPoints); % set to 0 any points that contain artifacts or just wish to ignore.
+
+
+    %% look at bursts
+    BandNames = fieldnames(Bands);
+
+    EEG = struct();
+    EEG.data = Signal;
+    EEG.srate = fs;
+    FiltEEG = EEG;
+
+    for Indx_F = 1:numel(BandNames)
+        B = Bands.(BandNames{Indx_F});
+        fSignal = hpfilt(Signal, fs, B(1));
+        fSignal = lpfilt(fSignal, fs, B(2));
+
+        % count bursts
+        FiltEEG(Indx_F).data = fSignal;
+    end
 
     FinalBursts = getAllBursts(EEG, FiltEEG, BurstThresholds, Min_Peaks, Bands, Keep_Points);
 
+    NBursts(Indx_R) = numel(FinalBursts);
+    periods = cat(2, periods, FinalBursts.period);
+    npeaks =  cat(2, npeaks, FinalBursts.nPeaks);
+
+end
 
 
+%%
 
 
+figure('Units','normalized', 'Position',[0 0 1 .5])
+subplot(1, 3, 1)
+histogram(NBursts)
+title(['Mean: ', num2str(mean(NBursts)), '; 5%: ', num2str(quantile(NBursts, .95))])
+
+subplot(1, 3, 1)
+histogram(NBursts)
+title(['Mean: ', num2str(mean(NBursts)), '; 5%: ', num2str(quantile(NBursts, .95))])
+
+
+subplot(1, 3, 1)
+histogram(NBursts)
+title(['Mean: ', num2str(mean(NBursts)), '; 5%: ', num2str(quantile(NBursts, .95))])
+saveas(gcf,  [num2str(Repeats), '_Diagnostics.jpg']);
 
 
 
