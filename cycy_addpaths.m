a40a4aeca90974c1588843434d718da56c59ddc7
Sophia Snipes <code@sophia.science>
2023-07-22 18:56:57 +0200

    one last filename change

---
+++ cycy_addpaths.m
@@ -0,0 +1,15 @@
+function cycy_addpaths()
+% add all folders to path so scripts can find functions
+
+
+CD = mfilename('fullpath');
+CD = extractBefore(CD, 'cycy_addpaths');
+
+% get all folders in matcyle directory
+Subfolders = deblank(string(ls(fullfile(CD, 'src')))); % all content
+Subfolders(contains(Subfolders, '.')) = []; % remove all files
+Subfolders(contains(Subfolders, 'private')) = []; % remove private folder
+
+for Indx_F = 1:numel(Subfolders)
+    addpath(fullfile(CD, Subfolders{Indx_F}))
+end
