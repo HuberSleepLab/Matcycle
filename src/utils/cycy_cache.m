7aaecfe724decc1ed292827c123c2e9044f33be8
Sophia Snipes <code@sophia.science>
2023-07-29 15:52:22 +0200

    created caching script

---
+++ src/utils/cycy_cache.m
@@ -0,0 +1,48 @@
+function Output = cycy_cache(Function, varargin)
+% runs a function, saves the output in a cache, and next time the function
+% is run, if there's something in the cache, it just uses that
+
+% saves the cache folder in folder where this script is
+ScriptPath = mfilename('fullpath');
+ScriptDir = extractBefore(ScriptPath, 'cycy_cache');
+
+StringInput = string_all_input(Function, varargin{:});
+
+CacheDir = fullfile(ScriptDir, 'cache');
+CachePath = fullfile(CacheDir, [StringInput, '.mat']);
+
+if exist(CachePath, 'file')
+    load(CachePath, 'Output')
+    return
+elseif ~exist(CacheDir, 'dir')
+    mkdir(CacheDir)
+end
+
+% run function with all its inputs
+disp(['Running ', func2str(Function), '...'])
+Output = Function(varargin{:});
+
+% save to cache
+save(CachePath, 'Output')
+
+end
+
+
+
+function StringInput = string_all_input(Function, varargin)
+% strings together all the inputs, so that it gets saved in a filename
+
+StringInput = {func2str(Function)};
+for Indx_V = 1:numel(varargin)
+
+    Item = varargin{Indx_V};
+    if isnumeric(Item)
+        Item = string(Item);
+    end
+
+    StringInput = cat(2, StringInput, Item);
+end
+
+StringInput = char(strjoin(StringInput, '_'));
+
+end
