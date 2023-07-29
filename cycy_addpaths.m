fb3f6e34d74c630b14b1213037a63791bb4d5f72
Sophia Snipes <code@sophia.science>
2023-07-29 11:48:46 +0200

    add path

--- cycy_addpaths.m
+++ cycy_addpaths.m
@@ -10,6 +10,7 @@ Subfolders = deblank(string(ls(fullfile(CD, 'src')))); % all content
 Subfolders(contains(Subfolders, '.')) = []; % remove all files
 Subfolders(contains(Subfolders, 'private')) = []; % remove private folder
 
+addpath(fullfile(CD, 'src'))
 for Indx_F = 1:numel(Subfolders)
     addpath(fullfile(CD, 'src', Subfolders{Indx_F}))
 end
