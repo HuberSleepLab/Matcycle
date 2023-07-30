d6c22148134e641be1bfeb0e1b4bf3bf7585d71b
Sophia Snipes <code@sophia.science>
2023-07-30 11:30:49 +0200

    rename ariables

--- src/cycy_detect_cycles.m
+++ src/cycy_detect_cycles.m
@@ -1,33 +1,28 @@
-function Peaks = cycy_detect_cycles(Wave, fWave)
-% detects peaks in signal based on zero-crossings. Wave should be only
-% loosely filtered (e.g. 3-40 Hz). The data gets further filtered just to
-% find the zero-crossings. FreqRange should be like [3 12].
-% Output includes all the positive peaks and negative peaks between
-% zero-crossings. Then MidDown and Up are the midpoints in amplitude
-% between consecutive peaks.
-% The output is a struct containing all peaks, based on the negative
-% peak and following positive peak.
+function Cycles = cycy_detect_cycles(ChannelBroadband, ChannelNarrowband)
+% detects peaks in narrowband filtered data based on zero-crossings.
+% The output is a struct containing all cycles in the channel, from a
+% midpoint between positive and negative peaks to the next.
 %
 % Part of Matcycle 2022, by Sophia Snipes.
 
-[FallingEdgeZeroCrossing, RisingEdgeZeroCrossing] = detect_zero_crossings(fWave);
+[FallingEdgeZeroCrossing, RisingEdgeZeroCrossing] = detect_zero_crossings(ChannelNarrowband);
 
 %%% Find peaks and troughs between zero crossings
-Peaks = cycy_detect_peaks(FallingEdgeZeroCrossing, RisingEdgeZeroCrossing, Wave);
+Cycles = cycy_detect_peaks(FallingEdgeZeroCrossing, RisingEdgeZeroCrossing, ChannelBroadband);
 
 % final adjustment to positive peaks to make sure they are the largest
 % point between midpoints. % TODO also for negative??
-for n = 1:numel(Peaks)-1
-    [~, PosPeakID] = max(Wave(Peaks(n).MidUpID:Peaks(n+1).MidDownID));
-    PosPeakID = PosPeakID + Peaks(n).MidUpID - 1;
-    Peaks(n).PosPeakID = PosPeakID;
-    Peaks(n).NextMidDownID = Peaks(n+1).MidDownID;
+for n = 1:numel(Cycles)-1
+    [~, PosPeakID] = max(ChannelBroadband(Cycles(n).MidUpID:Cycles(n+1).MidDownID));
+    PosPeakID = PosPeakID + Cycles(n).MidUpID - 1;
+    Cycles(n).PosPeakID = PosPeakID;
+    Cycles(n).NextMidDownID = Cycles(n+1).MidDownID;
     if n>1
-        Peaks(n).PrevPosPeakID = Peaks(n-1).PosPeakID;
+        Cycles(n).PrevPosPeakID = Cycles(n-1).PosPeakID;
     end
 end
 
 % remove last peak if it's positive peak doesn't exist:
-if Peaks(n).PosPeakID > numel(Wave)
-    Peaks(end) = [];
+if Cycles(n).PosPeakID > numel(ChannelBroadband)
+    Cycles(end) = [];
 end
