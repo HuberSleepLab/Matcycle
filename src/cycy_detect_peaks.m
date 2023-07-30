454e527d19515dff6726fa5f45578a5f7ea7b017
Sophia Snipes <code@sophia.science>
2023-07-30 11:54:35 +0200

    start separating peaks and midpoint detection

--- src/cycy_detect_peaks.m
+++ src/cycy_detect_peaks.m
@@ -3,55 +3,61 @@ function Peaks = cycy_detect_peaks(FallingEdgeZeroCrossings, RisingEdgeZeroCross
 % Part of Matcycle 2022, by Sophia Snipes.
 
 Peaks = struct();
-for n = 1:length(FallingEdgeZeroCrossings)
+for idxPeak = 1:length(FallingEdgeZeroCrossings)
+    %%% find negative peaks
 
     % find lowest point between zero crossings
-    [NegPeakAmplitude, RelativeNegPeakIdx] = min(ChannelBroadband(FallingEdgeZeroCrossings(n):RisingEdgeZeroCrossings(n)));
+    [NegPeakAmplitude, RelativeNegPeakIdx] = min(ChannelBroadband(FallingEdgeZeroCrossings(idxPeak):RisingEdgeZeroCrossings(idxPeak)));
 
     % adjust negative peak index to absolute value in channel signal
-    NegPeakIdx = RelativeNegPeakIdx + FallingEdgeZeroCrossings(n) - 1;
+    NegPeakIdx = RelativeNegPeakIdx + FallingEdgeZeroCrossings(idxPeak) - 1;
 
-    Peaks(n).NegPeakIdx = NegPeakIdx;
+    Peaks(idxPeak).NegPeakIdx = NegPeakIdx;
+
+    %%% find positive peaks
+    if idxPeak < length(FallingEdgeZeroCrossings)
+        [PosPeakAmp, PosPeakID] = max(ChannelBroadband(RisingEdgeZeroCrossings(idxPeak):FallingEdgeZeroCrossings(idxPeak+1)));
+        PosPeakID = PosPeakID + RisingEdgeZeroCrossings(idxPeak) - 1;
+        Peaks(idxPeak).PosPeakIdx = PosPeakID;
+    else 
+        % the last cycle needs special treatment:
+        % the positive peak is just the point right after the zero-crossing
+        Peaks(idxPeak).PosPeakIdx = RisingEdgeZeroCrossings(idxPeak)+1;
+    end
 
 
-    % same for positive peak
-    if n < length(FallingEdgeZeroCrossings)
-        [PosPeakAmp, PosPeakID] = max(ChannelBroadband(RisingEdgeZeroCrossings(n):FallingEdgeZeroCrossings(n+1)));
-        PosPeakID = PosPeakID + RisingEdgeZeroCrossings(n) - 1;
-        Peaks(n).PosPeakIdx = PosPeakID;
+    %%% Find midpoints
 
-        %%% Find midpoints (first point that crosses midpoint)
-
-        % after peak
+    % find midpoints between negative and positive zero-crossings
+    if idxPeak < length(FallingEdgeZeroCrossings)
         MidUpAmp = NegPeakAmplitude + (PosPeakAmp-NegPeakAmplitude)/2;
         halfWave = ChannelBroadband(NegPeakIdx:PosPeakID);
         MidUpID = find([halfWave nan] >= MidUpAmp & [nan halfWave] < MidUpAmp, 1, 'first'); % shift by one and see where crossing happens
         if isempty(MidUpID)
-            MidUpID = RisingEdgeZeroCrossings(n);
+            MidUpID = RisingEdgeZeroCrossings(idxPeak);
         else
             MidUpID = MidUpID + NegPeakIdx - 1;
         end
-        Peaks(n).MidRisingIdx = MidUpID;
+        Peaks(idxPeak).MidRisingIdx = MidUpID;
     else
-        Peaks(n).PosPeakIdx =RisingEdgeZeroCrossings(n)+1;
-        Peaks(n).MidRisingIdx = RisingEdgeZeroCrossings(n);
+        Peaks(idxPeak).MidRisingIdx = RisingEdgeZeroCrossings(idxPeak);
     end
 
-    % before peak
-    if n > 1
-        PrevPosPeakID = Peaks(n-1).PosPeakIdx;
+    % find midpoints between positive and negative zero-crossings
+    if idxPeak > 1
+        PrevPosPeakID = Peaks(idxPeak-1).PosPeakIdx;
         PrevPosAmp = ChannelBroadband(PrevPosPeakID);
         MidDownAmp = PrevPosAmp + (NegPeakAmplitude-PrevPosAmp)/2;
         halfWave = ChannelBroadband(PrevPosPeakID:NegPeakIdx);
         MidDownID = find([nan halfWave] > MidDownAmp & [halfWave nan] <= MidDownAmp, 1, 'last');
         if isempty(MidDownID)
-            MidDownID = FallingEdgeZeroCrossings(n);
+            MidDownID = FallingEdgeZeroCrossings(idxPeak);
         else
             MidDownID = MidDownID + PrevPosPeakID - 1;
         end
-        Peaks(n).MidFallingIdx = MidDownID;
+        Peaks(idxPeak).MidFallingIdx = MidDownID;
     else
-        Peaks(n).MidFallingIdx = FallingEdgeZeroCrossings(n);
+        Peaks(idxPeak).MidFallingIdx = FallingEdgeZeroCrossings(idxPeak);
     end
 end
 
