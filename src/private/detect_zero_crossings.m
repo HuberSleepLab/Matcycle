c55344600ddab2cba19e1121a8a30b3f6a1a7e71
Sophia Snipes <code@sophia.science>
2023-07-30 11:30:30 +0200

    cleanup zero-crossing

--- src/private/detect_zero_crossings.m
+++ src/private/detect_zero_crossings.m
@@ -1,25 +1,28 @@
-function [FallingEdgeZeroCrossing, RisingEdgeZeroCrossing] = detect_zero_crossings(Signal)
-% gets all points in which the signal does from positive to negative (down
-% zero crossing) and from negative to positive (up zero crossing)
+function [FallingEdgeCrossings, RisingEdgeCrossings] = detect_zero_crossings(Signal)
+% gets all points in which the signal goes from positive to negative values
+% (falling edge zero-crossing) and from negative to positive (rising edge
+% zero-crossing).
+%
+% Part of Matcycle 2022, by Sophia Snipes.
 
-% get when signal positive and negative
-signData = sign(Signal);
+SignSignal = sign(Signal);
 
-% fix rare issue where slope is exactly 0
-signData(signData == 0) = 1;
+% fix edgecase where slope is exactly 0
+SignSignal(SignSignal == 0) = 1;
 
-% -2 indicates when the sign goes from 1 to -1
-FallingEdgeZeroCrossing = find(diff(signData) < 0);
-RisingEdgeZeroCrossing = find(diff(signData) > 0);
+FallingEdgeCrossings = find(diff(SignSignal) < 0);
+RisingEdgeCrossings = find(diff(SignSignal) > 0);
 
-RisingEdgeZeroCrossing = RisingEdgeZeroCrossing + 1;
+% attributes the rising edge zero-crossing index to the right-side datapoint
+RisingEdgeCrossings = RisingEdgeCrossings + 1;
 
-% Check for earlier initial UZC than DZC
-if RisingEdgeZeroCrossing(1) <= FallingEdgeZeroCrossing(1)
-    RisingEdgeZeroCrossing(1)=[];
+% Ensure that the first index is always a falling edge zero-crossing
+if RisingEdgeCrossings(1) <= FallingEdgeCrossings(1)
+    RisingEdgeCrossings(1) = [];
 end
 
-% in case the last DZC does not have a corresponding UZC then delete it
-if length(FallingEdgeZeroCrossing) ~= length(RisingEdgeZeroCrossing)
-    FallingEdgeZeroCrossing(end)=[];
+% in case the last falling edge zero-crossing is not followed by a rising
+% edge zero-crossing, then delete it
+if length(FallingEdgeCrossings) ~= length(RisingEdgeCrossings)
+    FallingEdgeCrossings(end) = [];
 end
