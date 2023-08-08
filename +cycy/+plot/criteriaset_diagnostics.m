c2d4ad44e6fabb3c76a1f333b74a7c737c7d79ca
Sophia Snipes <snipeso@users.noreply.github.com>
2023-08-08 17:33:30 +0200

    plot diagnostics

--- +cycy/+plot/criteriaset_diagnostics.m
+++ +cycy/+plot/criteriaset_diagnostics.m
@@ -1,2 +1,19 @@
 function criteriaset_diagnostics(Diagnostics)
 
+CriteriaLabels = fieldnames(Diagnostics);
+isUnique = contains(CriteriaLabels, 'Unique');
+CriteriaLabelsUnique = CriteriaLabels(isUnique);
+CriteriaLabelsUnique = replace(CriteriaLabelsUnique, 'Unique', '');
+
+CriteriaLabels(isUnique) = [];
+Diagnostics = cell2mat(struct2cell(Diagnostics));
+
+
+figure('Units','normalized', 'position', [0 0 .5 .5])
+subplot(1,2,1)
+bar(categorical(CriteriaLabels), Diagnostics(~isUnique))
+title('Cycles disqualified')
+
+subplot(1,2,2)
+bar(categorical(CriteriaLabelsUnique), Diagnostics(isUnique))
+title('Cycles uniquely disqualified')
