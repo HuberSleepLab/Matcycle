f6f4b6be1aacc458974bf0211b247d12ff13fc9a
Sophia Snipes <snipeso@users.noreply.github.com>
2023-08-09 16:31:46 +0200

    moved functions into main function script

--- +cycy/detect_cycles.m
+++ +cycy/detect_cycles.m
@@ -5,14 +5,11 @@ function Cycles = detect_cycles(ChannelBroadband, ChannelNarrowband)
 % Part of Matcycle 2022, by Sophia Snipes.
 
 [RisingEdgeZeroCrossings, FallingEdgeZeroCrossings] = ...
-    cycy.detect_zero_crossings(ChannelNarrowband);
+    detect_zero_crossings(ChannelNarrowband);
 
-try
-[NegPeaks, PosPeaks] = cycy.detect_peaks(RisingEdgeZeroCrossings, ...
+[NegPeaks, PosPeaks] = detect_peaks(RisingEdgeZeroCrossings, ...
     FallingEdgeZeroCrossings, ChannelBroadband);
-catch
-    a=1
-end
+
 
 NegPeaksCount = numel(NegPeaks);
 Cycles = struct();
@@ -20,4 +17,61 @@ for idxPeak = 1:NegPeaksCount
     Cycles(idxPeak).NegPeakIdx = NegPeaks(idxPeak);
     Cycles(idxPeak).PrevPosPeakIdx = PosPeaks(idxPeak);
     Cycles(idxPeak).NextPosPeakIdx = PosPeaks(idxPeak+1);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% functions
+
+function [RisingEdgeCrossings, FallingEdgeCrossings] = detect_zero_crossings(Signal)
+% finds all the timepoints in which the signal goes from positive to 
+% negative (falling edge zero-crossing) and from negative to positive
+% (rising edge zero-crossing).
+% Ensures that the first rising edge zero-crossing comes at an earlier
+% timepoint than the first falling-edge zero-crossing (ensures that the 
+% sequence starts with a positive cycle).
+
+[RisingEdgeCrossings, FallingEdgeCrossings] = detect_crossings(Signal, 0);
+
+% Ensure that the first index is always a rising edge zero-crossing
+if RisingEdgeCrossings(1) > FallingEdgeCrossings(1)
+    FallingEdgeCrossings(1) = [];
+elseif RisingEdgeCrossings(1) == FallingEdgeCrossings(1)
+    FallingEdgeCrossings(1) = FallingEdgeCrossings(1)+1;
+
+end
+
+% Ensure that the last index is always a falling edge zero-crossing
+RisingEdgeCrossings = RisingEdgeCrossings(1:length(FallingEdgeCrossings));
+
+
+function [NegPeaks, PosPeaks] = detect_peaks(RisingEdgeZeroCrossings, ...
+    FallingEdgeZeroCrossings, ChannelBroadband)
+% Finds the negative and positive peaks between zero-crossings. NegPeaks
+% and PosPeaks are (2 x number of peaks) arrays, with peak indexes in the first
+% row, and amplitudes in the second.
+% Caution: the first rising edge zero-crossing must come at an earlier
+% timepoint than the first falling edge zero-crossing (sequence starts with
+% a positive cycle).
+
+PeaksCount = numel(RisingEdgeZeroCrossings);
+PosPeaks = nan([1, PeaksCount]);
+NegPeaks = nan([1, PeaksCount-1]);
+
+for idxPeak = 1:PeaksCount
+
+    %%% find positive peaks
+    [~, RelativePosPeakIdx] = max(ChannelBroadband(...
+        RisingEdgeZeroCrossings(idxPeak):FallingEdgeZeroCrossings(idxPeak)));
+    
+    PosPeaks(idxPeak) = RelativePosPeakIdx + RisingEdgeZeroCrossings(idxPeak) - 1;
+
+
+    %%% find negative peaks
+    % The signal always ends with a positive peak, so we stop one short
+    if idxPeak < PeaksCount
+        [~, RelativeNegPeakIdx] = min(ChannelBroadband(...
+            FallingEdgeZeroCrossings(idxPeak):RisingEdgeZeroCrossings(idxPeak+1)));
+        
+        NegPeaks(idxPeak) = RelativeNegPeakIdx + FallingEdgeZeroCrossings(idxPeak) - 1;
+    end
 end
