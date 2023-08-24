87993844f15bfefa118a289d972d6a45d29884b2
Sophia Snipes <snipeso@users.noreply.github.com>
2023-08-24 23:55:41 +0200

    plot also bursts power

--- +cycy/+plot/power_without_bursts.m
+++ +cycy/+plot/power_without_bursts.m
@@ -4,24 +4,32 @@ function power_without_bursts(DataBroadband, SampleRate, Bursts)
 % plot power spectrum of channel
 [PowerBroadband, Frequencies] = cycy.utils.compute_power(DataBroadband, SampleRate);
 hold on
-cycy.plot.power_spectrum(PowerBroadband, Frequencies, true, true, [], cycy.utils.pick_colors(1, '', 'blue'));
+cycy.plot.power_spectrum(PowerBroadband, Frequencies, true, true, [], [0 0 0]);
 
 
 % plot spectrum after removing bursts
 BurstStarts = [Bursts.Start];
 BurstEnds = [Bursts.End];
 
-Remove = zeros(size(DataBroadband));
+isBurst = false(size(DataBroadband));
 for idxBurst = 1:numel(BurstStarts)
-    Remove(BurstStarts(idxBurst):BurstEnds(idxBurst)) = 1;
+    isBurst(BurstStarts(idxBurst):BurstEnds(idxBurst)) = 1;
 end
 
-DataBurstless = DataBroadband(~Remove);
+DataBurst = DataBroadband(isBurst);
+[PowerBroadband, Frequencies] = cycy.utils.compute_power(DataBurst, SampleRate);
+hold on
+cycy.plot.power_spectrum(PowerBroadband, Frequencies, true, true, [], cycy.utils.pick_colors(1, '', 'blue'));
 
+
+DataBurstless = DataBroadband(~isBurst);
 [PowerBroadband, Frequencies] = cycy.utils.compute_power(DataBurstless, SampleRate);
 hold on
 cycy.plot.power_spectrum(PowerBroadband, Frequencies, true, true, [], cycy.utils.pick_colors(1, '', 'red'));
 
-disp(['Bursts occupied ', num2str(round(100*nnz(Remove)/numel(Remove))), '% of the EEG'])
+
+
+
+disp(['Bursts occupied ', num2str(round(100*nnz(isBurst)/numel(isBurst))), '% of the EEG'])
 
 % TODO: apply to larger dataset: multi channels; multi participants
