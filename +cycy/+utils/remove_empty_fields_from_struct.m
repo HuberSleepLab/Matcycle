c55c7bacba0373a5370bba1c420ead2b326566fb
Sophia Snipes <snipeso@users.noreply.github.com>
2023-08-29 20:56:37 +0200

    handle edge case

--- +cycy/+utils/remove_empty_fields_from_struct.m
+++ +cycy/+utils/remove_empty_fields_from_struct.m
@@ -1,6 +1,11 @@
 function Struct = remove_empty_fields_from_struct(Struct)
 % removes empty fields
 
+if isempty(Struct)
+    warning('empty struct')
+    return
+end
+
 Fieldnames  = fieldnames(Struct);
 
 for Indx_F = 1:numel(Fieldnames)
