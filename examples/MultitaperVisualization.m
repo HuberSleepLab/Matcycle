9b0daa65776a19d791609b234ab8c8f2fb6c1a4b
Sophia Snipes <snipeso@users.noreply.github.com>
2024-06-28 17:45:00 +0200

    played around

--- examples/MultitaperVisualization.m
+++ examples/MultitaperVisualization.m
@@ -6,17 +6,21 @@ close all
 Filepath = 'E:\Data\Preprocessed\Clean\Waves\Music\P07_Music_Session2_Clean.mat';
 % Filepath = 'E:\Data\Preprocessed\Clean\Waves\Game\P10_Game_Session2_Clean.mat';
 
+
+
+%%
 % Choose window to look at
 Window = [80 95]; % seconds
 Window = [0 400];
-WindowLength = 1;
+Window = [0 300];
+WindowLength = 3;
 MovingWindowSampleRate = .1;
 
-load(Filepath, 'EEG')
+% load(Filepath, 'EEG')
 
 % reduce data to make it faster
 EEGRedux = pop_select(EEG, 'time', Window);
-EEGRedux = pop_select(EEGRedux, 'channel', [6 11 36 51 68]); % 6, Fz, C3, P3 O1
+EEGRedux = pop_select(EEGRedux, 'channel', [117]); % 6, Fz, C3, P3 O1
 
 % run multitaper
 Data = EEGRedux.data;
@@ -25,8 +29,7 @@ SampleRate = EEG.srate;
 [Spectrum, Frequencies, Time] = cycy.utils.multitaper(Data, SampleRate, WindowLength, MovingWindowSampleRate);
 
 
-%% plot
-
+% plot
 PlotProps = chART.load_plot_properties({'LSM', 'Manuscript'});
 ChannelLabels = {EEGRedux.chanlocs.labels};         
 
@@ -35,7 +38,7 @@ for ChannelIdx = 1:size(Spectrum, 1)
     LData = squeeze(log(Spectrum(ChannelIdx, :, :)));
 
     subplot(size(Spectrum, 1), 1, ChannelIdx)
-    cycy.plot.time_frequency(LData, Frequencies, Time(end), 'contourf', [1 40], [-4, -2], 100)
+    cycy.plot.time_frequency(LData, Frequencies, Time(end), 'contourf', [1 40], [-10, -2], 100)
     chART.set_axis_properties(PlotProps)
     colormap(PlotProps.Color.Maps.Linear)
     title(ChannelLabels(ChannelIdx))
