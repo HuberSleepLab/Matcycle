a9cca5ec3047044d98f104bdc25399782e6ae9a0
Sophia Snipes <snipeso@users.noreply.github.com>
2024-06-28 14:14:13 +0200

    move mask bursts external function

--- +cycy/+plot/plot_all_bursts.m
+++ +cycy/+plot/plot_all_bursts.m
@@ -36,7 +36,7 @@ for idxCategory = 1:numel(CategoryLabels)
     end
 
     % get data for each burst category
-    [BurstMask, ReferenceMask] = mask_bursts(EEG.data, Bursts(BurstIndexes));
+    [BurstMask, ReferenceMask] = cycy.utils.mask_bursts(EEG.data, Bursts(BurstIndexes));
 
     % plot bursts
     if isempty(ReferenceMask)
@@ -54,47 +54,6 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% functions
 
-function [BurstMask, ReferenceMask] = mask_bursts(EEGData, Bursts)
-% creates a matrix the size of the data in EEG, with values of the data
-% only during the bursts
-
-BurstMask = nan(size(EEGData));
-
-% identify location of bursts
-if isfield(Bursts, 'ClusterStart')
-    Starts = [Bursts.ClusterStarts];
-    Ends = [Bursts.ClusterEnds];
-    Channels = [Bursts.ClusterChannelIndexes];
-else
-    Starts = [Bursts.Start];
-    Ends = [Bursts.End];
-    Channels = [Bursts.ChannelIndex];
-end
-
-% move data from EEG to mask
-for idxBursts = 1:numel(Starts)
-    BurstMask(Channels(idxBursts), Starts(idxBursts):Ends(idxBursts)) = ...
-        EEGData(Channels(idxBursts), Starts(idxBursts):Ends(idxBursts));
-end
-
-% create seperate mask just for reference channels when burst clusters are
-% provided, so that they can be made thicker
-if ~isfield(Bursts, 'ClusterStart')
-    ReferenceMask = [];
-    return
-end
-
-ReferenceMask = nan(size(EEGData));
-
-RefStarts = [Bursts.Start];
-RefEnds = [Bursts.End];
-RefChannels = [Bursts.ChannelIndex];
-
-for idxRefBursts = 1:numel(RefStarts)
-   ReferenceMask(RefChannels(idxRefBursts), RefStarts(idxRefBursts):RefEnds(idxRefBursts)) = ...
-        EEGData(RefChannels(idxRefBursts), RefStarts(idxRefBursts):RefEnds(idxRefBursts));
-end
-end
 
 
 function [Colors, CategoryLabels] = pick_group_colors(Bursts, ColorCategory)
