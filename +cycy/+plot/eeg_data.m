55407fb8c7651d47cb736ef19dc165c66d8fe84d
Sophia Snipes <snipeso@users.noreply.github.com>
2024-12-10 22:33:57 +0100

    moved function from other branch

--- +cycy/+plot/eeg_data.m
+++ +cycy/+plot/eeg_data.m
@@ -3,35 +3,35 @@ arguments
     Data
     SampleRate
     YGap = 20;
-        DisplayName = '';
+    DisplayName = '';
     Color = [.3 .3 .3];
     LineWidth = 0.5;
 end
 % Data is ch x time matrix
 % YGap is how much to space the channels. Default is 20.
 
-    
 [ChannelCount, TimepointCount] = size(Data);
 
-Timepoints = linspace(0, TimepointCount/SampleRate, TimepointCount);
+Time = linspace(0, TimepointCount/SampleRate, TimepointCount);
 
+% spread out channels along the y axis
 YAxisGaps = YGap*ChannelCount:-YGap:0;
 YAxisGaps(end) = [];
-
 DataSpread = Data+YAxisGaps';
 
-% plot a single channel for the legend (hack)
+% plot a single point for the legend (hack)
 hold on
 if ~isempty(DisplayName)
-plot(Timepoints(1), DataSpread(1, 1), 'Color', Color, 'LineWidth', LineWidth, ...
-    'HandleVisibility', 'on', 'DisplayName', DisplayName)
+    plot(Time(1), DataSpread(1, 1), 'Color', Color, 'LineWidth', LineWidth, ...
+        'HandleVisibility', 'on', 'DisplayName', DisplayName)
 end
 
-plot(Timepoints, DataSpread,  'Color', Color, 'LineWidth', LineWidth, ...
+% main plot
+plot(Time, DataSpread,  'Color', Color, 'LineWidth', LineWidth, ...
     'HandleVisibility', 'off')
 
 YLims = [median(DataSpread(end, :))-YGap, median(DataSpread(1, :))+YGap];
 
 if all(~isnan(YLims))
-ylim(YLims)
+    ylim(YLims)
 end
